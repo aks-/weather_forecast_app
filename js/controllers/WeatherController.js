3460534ab2c783a31e261b045eb458043c7a0489
aks- <coderatlabs@gmail.com>
2015-04-12 22:17:12 +0530

    Add geolocation and input city support
    *Added gelocation search for city and find the weather accordingly
    *Added functionality to get the data from the service and render it

--- js/controllers/WeatherController.js
+++ js/controllers/WeatherController.js
@@ -1,5 +1,52 @@
-app.controller('WeatherController', ['$scope', '$timeout', 'WeatherService', function($scope, $timeout, WeatherService) {
-    $scope.cityName = WeatherService.success(function(data) {
-        return data.city;
-    });
+app.controller('WeatherController', ['$scope', '$timeout','WeatherService',
+        function($scope, $timeout, WeatherService) {
+
+        $scope.cityName = "loading...";
+        $scope.places = {};
+        $scope.forecast = {};
+        $scope.unit = "°C";
+        $scope.currentTabId = 0;
+        var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+        var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+        WeatherService.getCurrnetCityName(function(cityname) {
+                $scope.cityName = cityname;
+                getForecast();
+        });
+
+        $scope.onKeyup = function(event) {
+                if(event.keyCode == 13){
+                        getForecast();
+                }
+        };
+
+        $scope.onPlaceClick = function(tabId) {
+                $scope.currentTabId = tabId;
+        }
+
+        $scope.onPlaceDelete = function(tabId) {
+                delete $scope.places[tabId];
+                if( $scope.places != null && $scope.currentTabId == tabId) {
+                        //console.log(Object.keys($scope.places));
+                        $scope.currentTabId = Object.keys($scope.places)[0];;//$scope.places[0];
+                }
+        }
+
+        function getForecast() {
+                WeatherService.getCityWeatherReport($scope.cityName,function(data) {
+                        console.log(data);
+                        if(data.cod != 404)
+                        $timeout(function() {
+                                angular.forEach(data.list,function(value,key) {
+                                        var theDate = new Date(value.dt * 1000);
+                                        dateString = new Date(theDate.toGMTString());
+                                        value.dt = dateString.getDate()+" "+months[dateString.getMonth()];
+                                });
+                                //$scope.forecast = data;
+                                $scope.cityName = "";
+                                $scope.currentTabId = new Date().getTime()+Math.floor((Math.random() * 100) + 1);
+                                $scope.places[$scope.currentTabId] = data;
+                        });
+                });
+        }
 }]);
--- js/services/WeatherService.js
+++ js/services/WeatherService.js
@@ -2,5 +2,51 @@ app.factory('WeatherService', ['$http', function($http) {
     var appId = "34dca9502c68e2c0c9c9995d245cc995";
     var count = 14;
 
-    return $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?units=metric&q=" + cityName + "&cnt=" + count + "&APPID=" + appId).success(console.log("Success"))
-}])
+    return {
+        getCityWeatherReport : function(cityName,success) {
+            var url = "http://api.openweathermap.org/data/2.5/forecast/daily?units=metric&";
+            url += "q="+cityName+"&";
+            url += "cnt="+count+"&";
+            url += "APPID="+appId;
+
+            function callOpenWeatherMap(success) {
+                $http.get(url).success(success);
+            }
+
+            callOpenWeatherMap(success);
+        },
+        getCurrnetCityName : function(callback) {
+            console.log("getCurrnetCityName");
+            var geocoder = new google.maps.Geocoder();
+            navigator.geolocation.getCurrentPosition(success, error, {
+                enableHighAccuracy: true,
+                timeout: 10000,
+                maximumAge: 0
+            });
+
+            function success(position) {
+                console.log("getCurrnetCityName: success");
+
+                var lat = position.coords.latitude;
+                var lng = position.coords.longitude;
+                var latLng = new google.maps.LatLng(lat, lng);
+
+                geocoder.geocode({
+                    'latLng': latLng
+                }, function(results, status) {
+                    console.log("getCurrnetCityName: result");
+                    //console.log(results);
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        var splittedResults = results[0].formatted_address.split(",");
+                        callback(splittedResults[splittedResults.length - 3]);
+                    }
+                });
+            }
+
+            function error(err){
+                console.log("getCurrnetCityName : error");
+                console.log(err);
+            }
+        }
+    }
+}]);
